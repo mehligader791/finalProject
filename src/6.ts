754eef01aea580a8e4c8e65d316f6b9ba142e5d4
mehligader791 <mehligader791@gmail.com>
2025-03-11 15:43:43 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-final-project',
+  templateUrl: './final-project.component.html',
+  styleUrls: ['./final-project.component.css']
+})
+export class FinalProjectComponent{
+  title = 'Final Project';
+  problemStatement = "Problem Statement";
+  approachTaken = "Approach Taken";
+  findings = "Findings";
+}
