d32f6d0a217f7770d9346f29047699374ceac26f
mehligader791 <mehligader791@gmail.com>
2025-03-13 11:22:05 +0000

    Add 7.ts

---
+++ src/7.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finalProject',
+  templateUrl: './finalProject.component.html',
+  styleUrls: ['./finalProject.component.css']
+})
+export class FinalProjectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
