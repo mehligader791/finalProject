1658090f9a839c5d871bced3fb0f52bc4aa35f91
mehligader791 <mehligader791@gmail.com>
2025-03-19 13:51:08 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function FinalProject() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+
+export default FinalProject;
