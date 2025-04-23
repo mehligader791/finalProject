255d0a3ebcfb74b9553fa0019019906ec71d2c08
mehligader791 <mehligader791@gmail.com>
2025-04-23 11:12:20 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,10 @@
+let x = 5;
+if (x > 10) {
+    console.log('The value of x is greater than 10.');
+} else if (x < -5) {
+    console.log('The value of x is less than -5.');
+}
+
+// Optional: Adding a more complex condition
+console.log(`The value of x is ${Math.sqrt(x)} squared. `);
+
